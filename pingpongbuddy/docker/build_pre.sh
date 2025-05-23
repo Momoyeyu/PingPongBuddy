216a67c39a2b413fc27ea6bde8e3b081368b6207
momoyeyu <momoyeyu@outlook.com>
2025-05-23 12:21:20 +0000

    fix docker script

--- pingpongbuddy/docker/build_pre.sh
+++ pingpongbuddy/docker/build_pre.sh
@@ -4,6 +4,6 @@ IMAGE_HUB="harbor.lab.bigai.site"
 IMAGE_HUB_USER=${IMAGE_HUB_USER}
 IMAGE_HUB_PASS=${IMAGE_HUB_PASS}
 IMAGE_SRC_PATH="./"
-docker build -f pingpongbuddy/docker/Dockerfile -t $IMAGE_HUB/$IMAGE_TAG:$IMAGE_TAG_VER -t $IMAGE_HUB/$IMAGE_TAG:latest $IMAGE_SRC_PATH
-
 docker build -f pingpongbuddy/docker/Dockerfile.pre -t harbor.lab.bigai.site/junjie-su/pingpongbuddy:$IMAGE_TAG_VER -t harbor.lab.bigai.site/junjie-su/pingpongbuddy:latest ./
+
+docker build -f pingpongbuddy/docker/Dockerfile -t $IMAGE_HUB/$IMAGE_TAG:$IMAGE_TAG_VER -t $IMAGE_HUB/$IMAGE_TAG:latest $IMAGE_SRC_PATH
